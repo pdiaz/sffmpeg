3f5ab9a2e5989c22149fe7121c6c8c1667a7b99f
Pedro Diaz <p.diaz@wemersive.com>
2017-06-11 17:31:13 +0200

    Added https support to ffmpeg

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ set -eo pipefail
 rm -rf ./ffmpeg-bin/sffmpeg_*.deb ../sffmpeg_*.deb
 
 docker build --tag=sffmpeg-builder .
-docker run -v $PWD/ffmpeg-bin:/ffmpeg-bin sffmpeg-builder:latest
+docker run --cpu-quota=800000 -v $PWD/ffmpeg-bin:/ffmpeg-bin sffmpeg-builder:latest
 cp ffmpeg-bin/sffmpeg_*.deb ../
